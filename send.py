e710bfb5e223ecc2ff76c289d1a34ceecccd7faf
XIANGYAO TANG <u1311496@umail.utah.edu>
2022-06-22 18:29:40 -0600

    change sending parameters

--- send.py
+++ send.py
@@ -9,8 +9,8 @@ class Sender:
         self.messages = self.createMessage(self.messageSize)
         # Interval of how much the sender needs to wait after finish one round of sending.
         self.waitTime = 5
-        self.secToRun = 120
-        self.numOfRepeat = 10
+        self.secToRun = 60
+        self.numOfRepeat = 5
         self.messageAmount = 0
         self.sendStarted = False
         self.credential = pika.PlainCredentials('sender', 'thisissender')
@@ -46,7 +46,7 @@ class Sender:
                     #     self.sendStarted = True
                     # Note: The rabbitmq server is responsible for flow control.
                     self.channel.basic_publish(exchange='bandwidthExperiment', routing_key = '', body = currentMessage)
-                    # self.messageAmount += 1
+                    self.messageAmount += 1
                 print("[x] Sent %s messages in %s seconds, each message is %s bytes" % (
                     self.messageAmount, 
                     self.secToRun, 
@@ -55,7 +55,7 @@ class Sender:
                 # self.channel.basic_publish(exchange='bandwidthExperiment', routing_key = '', body = 'x')
                 # self.sendStarted = False
 
-                # self.logData(currentMessageSize, self.messageAmount)
+                self.logData(currentMessageSize, self.messageAmount)
                 self.messageAmount = 0
                 print("[x] Now sleeping for 5 seconds.")
                 time.sleep(self.waitTime)
