451a8e135848870012b0e38d5b9ac3ddae564f9c
XIANGYAO TANG <u1311496@umail.utah.edu>
2022-06-22 19:16:35 -0600

    changed message Size

--- send.py
+++ send.py
@@ -5,7 +5,7 @@ import time
 class Sender:
     def __init__(self):
         # self.messageSize = [256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576, 2097152]
-        self.messageSize = [4096]
+        self.messageSize = [16384]
         self.messages = self.createMessage(self.messageSize)
         # Interval of how much the sender needs to wait after finish one round of sending.
         self.waitTime = 0.1
